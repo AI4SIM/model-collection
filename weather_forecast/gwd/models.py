e66b9b2c9b8190d3be547ebfbb763b575a9ba8d4
GIORKALLOS, Alexis <alexis.giorkallos@atos.net>
2021-12-21 16:24:56 +0000

    still in dev. implemented cnn for gwd.

--- weather_forecast/gwd/models.py
+++ weather_forecast/gwd/models.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pytorch_lightning as pl
 from pytorch_lightning.utilities.cli import MODEL_REGISTRY
+import torch
 import torch.nn as nn
 import torch_geometric as pyg
 import torch_optimizer as optim
@@ -55,6 +56,45 @@ class LitMLP(NOGWDModule):
             nn.Linear(hidden_channels, hidden_channels),
             nn.ReLU(inplace=True),
             nn.Linear(hidden_channels, out_channels),
+        )
+        
+    
+    def configure_optimizers(self):
+        return optim.AdamP(self.parameters(), lr=self.lr)
+
+
+@MODEL_REGISTRY
+class LitCNN(NOGWDModule):
+    
+    class Reshape(nn.Module):
+        def __init__(self):
+            super().__init__()
+            
+        def forward(self, x):
+            t0 = torch.reshape(x[:, :3 * 63], [-1, 3, 63])
+            t1 = torch.tile(x[:, 3 * 63:].unsqueeze(2), (1, 1, 63))
+            
+            return torch.cat((
+                torch.transpose(t0, -2, -1),
+                torch.transpose(t1, -2, -1)
+            ), dim=2)
+    
+    def __init__(self, in_channels, init_feat, conv_size, pool_size, out_channels, lr):
+        super().__init__()
+        self.save_hyperparameters()
+        
+        self.lr = lr
+        self.model = nn.Sequential(
+            self.Reshape(),
+            nn.Conv1d(in_channels=in_channels, out_channels=2 * init_feat, kernel_size=conv_size),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=pool_size),
+            nn.Conv1d(in_channels=2 * init_feat, out_channels=4 * init_feat, kernel_size=conv_size),
+            nn.ReLU(inplace=True),
+            nn.MaxPool1d(kernel_size=pool_size),
+            nn.Flatten(),
+            nn.Linear(4 * init_feat, out_channels),
+            nn.Linear(out_channels, out_channels)
         )
         
     
