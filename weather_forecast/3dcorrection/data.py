51f189ddd7cdf15b268621d1735a897b5ba9f1cf
BOVALO, Christophe <christophe.bovalo@atos.net>
2022-02-08 17:03:08 +0000

    added 3dcorrection use-case with first model on GNNs

---
+++ weather_forecast/3dcorrection/data.py
@@ -0,0 +1,171 @@
+import h5py
+from memory_profiler import profile
+import networkx as nx
+import numpy as np
+import os
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.cli import DATAMODULE_REGISTRY
+import torch
+import torch.nn.functional as F
+import torch_geometric as pyg
+import torch_optimizer as optim
+import yaml
+import netCDF4
+from tqdm import tqdm
+
+import config
+
+class ThreeDCorrectionDataset(pyg.data.InMemoryDataset):
+    
+    def __init__(self, root, step, force=False):
+        self.step = step
+        
+        super().__init__(root)
+        
+        path = self.processed_paths[0]
+        if force:
+            os.remove(path)
+            
+        self.data, self.slices = torch.load(path)
+
+    @property
+    def raw_file_names(self):
+        return [f"data-{self.step}.nc"]
+
+    @property
+    def processed_file_names(self):
+        return [f"data-{self.step}.pt"]
+    
+    @profile
+    def download(self):
+        import climetlab as cml
+
+        cml.settings.set("cache-directory", os.path.join(self.root, "raw"))
+
+        cmlds = cml.load_dataset(
+            'maelstrom-radiation', 
+            dataset='3dcorrection', 
+            raw_inputs=False, 
+            timestep=list(range(0, 3501, self.step)), 
+            minimal_outputs=False,
+            patch=list(range(0, 16, 1)),
+            hr_units='K d-1',
+        )
+        
+        array = cmlds.to_xarray()
+        array.to_netcdf(self.raw_paths[0])
+
+    @profile
+    def process(self):
+        
+        def broadcast_features(tensor):
+            t = torch.unsqueeze(tensor, -1)
+            t = t.repeat((1, 1, 138))
+            t = t.moveaxis(1, -1)
+            return t
+
+        def pad_tensor(tensor):
+            return F.pad(tensor, (0, 0, 1, 1, 0, 0))
+        
+        
+        i = 0
+        for raw_path in self.raw_paths:
+            
+            with netCDF4.Dataset(raw_path, "r", format="NETCDF4") as file:
+                sca_inputs = torch.tensor(file['sca_inputs'][:])
+                col_inputs = torch.tensor(file['col_inputs'][:])
+                hl_inputs = torch.tensor(file['hl_inputs'][:])
+                inter_inputs = torch.tensor(file['inter_inputs'][:])
+
+                flux_dn_sw = torch.tensor(file['flux_dn_sw'][:])
+                flux_up_sw = torch.tensor(file['flux_up_sw'][:])
+                flux_dn_lw = torch.tensor(file['flux_dn_lw'][:])
+                flux_up_lw = torch.tensor(file['flux_up_lw'][:])
+                
+            inter_inputs_ = pad_tensor(inter_inputs)
+            sca_inputs_ = broadcast_features(sca_inputs)
+
+            x = torch.cat([
+                hl_inputs,
+                inter_inputs_,
+                sca_inputs_
+            ], dim=-1)
+
+            y = torch.cat([
+                torch.unsqueeze(flux_dn_sw, -1),
+                torch.unsqueeze(flux_up_sw, -1),
+                torch.unsqueeze(flux_dn_lw, -1),
+                torch.unsqueeze(flux_up_lw, -1),
+            ], dim=-1)
+            
+            stats_path = os.path.join(self.root, f"stats-{self.step}.pt")
+            if not os.path.isfile(stats_path):
+                stats = {
+                    "x_mean" : torch.mean(x, dim=0),
+                    "y_mean" : torch.mean(y, dim=0),
+                    "x_std" : torch.std(x, dim=0),
+                    "y_std" : torch.std(y, dim=0)
+                }
+                torch.save(stats, stats_path)
+
+            directed_index = np.array([[*range(1, 138)], [*range(137)]])
+            undirected_index = np.hstack((
+                directed_index, 
+                directed_index[[1, 0], :]
+            ))
+            undirected_index = torch.tensor(undirected_index, dtype=torch.long)
+            
+            data_list = []
+            
+            for idx in tqdm(range(x.shape[0])):
+                x_ = torch.squeeze(x[idx, ...])
+                y_ = torch.squeeze(y[idx, ...])
+                
+                edge_attr = torch.squeeze(sca_inputs_[idx, ...])
+                
+                data = pyg.data.Data(
+                    x=x_,
+                    edge_attr=edge_attr,
+                    edge_index=undirected_index,
+                    y=y_,
+                )
+                
+                data_list.append(data)
+
+            torch.save(self.collate(data_list), self.processed_paths[0])
+
+
+@DATAMODULE_REGISTRY
+class LitThreeDCorrectionDataModule(pl.LightningDataModule):
+    """Creates datasets for the """
+    
+    def __init__(self, 
+                 timestep: int,
+                 batch_size: int,
+                 num_workers: int,
+                 force: bool = False):
+        
+        self.step = timestep
+        self.batch_size = batch_size
+        self.num_workers = num_workers
+        self.force = force
+        super().__init__()
+        
+    def prepare_data(self):
+        ThreeDCorrectionDataset(config.data_path, self.step, self.force)
+    
+    def setup(self, stage):
+        dataset = ThreeDCorrectionDataset(config.data_path, self.step, self.force).shuffle()
+        
+        self.test_dataset = dataset[1000000:]
+        self.val_dataset = dataset[900000:1000000]
+        self.train_dataset = dataset[:900000]
+    
+    def train_dataloader(self):
+        return pyg.loader.DataLoader(self.train_dataset, batch_size=self.batch_size, shuffle=True, num_workers=self.num_workers)
+    
+    def val_dataloader(self):
+        return pyg.loader.DataLoader(self.val_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
+    
+    def test_dataloader(self):
+        return pyg.loader.DataLoader(self.test_dataset, batch_size=self.batch_size, num_workers=self.num_workers)
---
+++ weather_forecast/3dcorrection/models.py
@@ -0,0 +1,167 @@
+import numpy as np
+import pytorch_lightning as pl
+from pytorch_lightning.utilities.cli import MODEL_REGISTRY
+import torch
+import torch.nn as nn
+import torch_geometric as pyg
+import torch_optimizer as optim
+import torchmetrics.functional as F
+import os
+
+import config
+
+class ThreeDCorrectionModule(pl.LightningModule):
+
+    def forward(self, x, edge_index):
+        return self.net(x, edge_index)
+
+    def histograms_adder(self):
+        for name, params in self.named_parameters():
+            self.logger.experiment.add_histogram(name, params, self.current_epoch)
+    
+    def _normalize(self, batch, batch_size):
+        stats = torch.load(os.path.join(config.data_path, f"stats-{self.step}.pt"))
+        device = self.device
+        x_mean = stats["x_mean"].to(device)
+        y_mean = stats["y_mean"].to(device)
+        x_std = stats["x_std"].to(device)
+        y_std = stats["y_std"].to(device)
+        
+        num_output_features = batch.y.size()[-1]
+        
+        x = (batch.x.reshape((batch_size, -1, batch.num_features)) - x_mean) / (x_std + torch.tensor(1.e-8))
+        # y = (batch.y.reshape((batch_size, -1, num_output_features)) - y_mean) / (y_std + torch.tensor(1.e-8))
+        
+        x = x.reshape(-1, batch.num_features)
+        # y = y.reshape(-1, num_output_features)
+        
+        return x, batch.y, batch.edge_index
+    
+    def _common_step(self, batch, batch_idx, stage, normalize=False):
+        batch_size = batch.ptr.size()[0]-1
+        
+        if normalize:
+            x, y, edge_index = self._normalize(batch, batch_size)
+        else:
+            x, y, edge_index = batch.x, batch.y, batch.edge_index
+        
+        y_hat = self(x, edge_index)
+        loss = F.mean_squared_error(y_hat, y)
+        r2 = F.r2_score(y_hat, y)
+
+        self.log(f"{stage}_loss", loss, prog_bar=True, on_step=True, batch_size=batch_size)
+        self.log(f"{stage}_r2", r2,  prog_bar=True, on_step=True, batch_size=batch_size)
+        
+        return y_hat, loss, r2
+
+    def training_step(self, batch, batch_idx):
+        _, loss, _ = self._common_step(batch,
+                                       batch_idx,
+                                       "train",
+                                       normalize=self.norm)
+        return loss
+    
+    def training_epoch_end(self, outputs):
+        #  the function is called after every epoch is completed
+        self.histograms_adder()
+
+    def validation_step(self, batch, batch_idx):
+        y_hat, _, _ = self._common_step(batch, batch_idx, "val")
+
+    def test_step(self, batch, batch_idx):
+        y_hat, _, _ = self._common_step(batch, batch_idx, "test")
+
+
+@MODEL_REGISTRY
+class LitGAT(ThreeDCorrectionModule):
+    
+    def __init__(self,
+                 in_channels,
+                 hidden_channels,
+                 out_channels,
+                 num_layers,
+                 dropout,
+                 heads,
+                 jk,
+                 lr,
+                 timestep,
+                 norm):
+        super().__init__()
+        self.save_hyperparameters()
+        
+        self.lr = lr
+        self.step = timestep
+        self.norm = norm
+        self.net = pyg.nn.GAT(in_channels=in_channels, 
+                                hidden_channels=hidden_channels,
+                                out_channels=out_channels,
+                                num_layers=num_layers,
+                                dropout=dropout,
+                                act=nn.SiLU(inplace=True),
+                                heads=heads,
+                                jk=jk)
+        
+    
+    def configure_optimizers(self):
+        return optim.AdamP(self.parameters(), lr=self.lr)
+
+
+# @MODEL_REGISTRY
+# class LitGCN(ThreeDCorrectionModule):
+    
+#     def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout, jk, lr):
+#         super().__init__()
+#         self.save_hyperparameters()
+        
+#         self.lr = lr
+#         self.model = pyg.nn.GCN(in_channels=in_channels, 
+#                                 hidden_channels=hidden_channels,
+#                                 out_channels=out_channels,
+#                                 num_layers=num_layers,
+#                                 dropout=dropout,
+#                                 jk=jk,
+#                                 act=nn.SiLU(inplace=True))
+        
+    
+#     def configure_optimizers(self):
+#         return optim.AdamP(self.parameters(), lr=self.lr)
+
+
+# @MODEL_REGISTRY
+# class LitGraphUNet(ThreeDCorrectionModule):
+    
+#     def __init__(self, in_channels, hidden_channels, out_channels, depth, pool_ratios, lr):
+#         super().__init__()
+#         self.save_hyperparameters()
+        
+#         self.lr = lr
+#         self.model = pyg.nn.GraphUNet(in_channels=in_channels, 
+#                                       hidden_channels=hidden_channels,
+#                                       out_channels=out_channels,
+#                                       depth=depth,
+#                                       pool_ratios=pool_ratios,
+#                                       act=nn.SiLU(inplace=True))
+        
+    
+#     def configure_optimizers(self):
+#         return optim.AdamP(self.parameters(), lr=self.lr)
+
+
+# @MODEL_REGISTRY
+# class LitGIN(ThreeDCorrectionModule):
+    
+#     def __init__(self, in_channels, hidden_channels, out_channels, num_layers, dropout, lr):
+#         super().__init__()
+#         self.save_hyperparameters()
+        
+#         self.lr = lr
+#         self.model = pyg.nn.GIN(in_channels=in_channels, 
+#                                 hidden_channels=hidden_channels,
+#                                 out_channels=out_channels,
+#                                 num_layers=num_layers,
+#                                 dropout=dropout,
+#                                 act=nn.SiLU(inplace=True))
+        
+    
+#     def configure_optimizers(self):
+#         return optim.AdamP(self.parameters(), lr=self.lr)
