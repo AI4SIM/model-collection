d09c780afddeebcb567d3c9b1a765abbced640d5
Lionel VINCENT <lionel.vincent.dm@atos.net>
2022-03-18 13:19:50 +0100

    trainer.py: add unit tests for the trainer module

--- weather_forecast/gwd/trainer.py
+++ weather_forecast/gwd/trainer.py
@@ -1,9 +1,9 @@
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -19,63 +19,61 @@ import torch
 from typing import List, Union
 
 import config
-import data
-import models
+
 
 class Trainer(pl.Trainer):
     """
-    Modified PyTorch Lightning Trainer that automatically tests, logs, and writes artifacts by the end of training.
+    Modified PyTorch Lightning Trainer that automatically tests, logs, and writes artifacts by the
+    end of training.
     """
-    
-    def __init__(self, 
+
+    def __init__(self,
                  accelerator: Union[str, pl.accelerators.Accelerator, None], 
-                 devices: Union[List[int], str, int, None], 
-                 max_epochs: int, 
-                 # TODO: delete. 
+                 devices: Union[List[int], str, int, None],
+                 max_epochs: int,
+                 # TODO: delete.
                  # For some reason, those two are mandatory in current version of Lightning.
-                 fast_dev_run: Union[int, bool] = False, 
+                 fast_dev_run: Union[int, bool] = False,
                  callbacks: Union[List[Callback], Callback, None] = None) -> None:
         """
         Args:
-            accelerator (Union[str, pl.accelerators.Accelerator, None]): Type of accelerator to use for training.
+            accelerator (Union[str, pl.accelerators.Accelerator, None]): Type of accelerator to use
+                for training.
             devices: (Union[List[int], str, int, None]): Devices explicit names to use for training.
             max_epochs (int): Maximum number of epochs if no early stopping logic is implemented.
         """
-        
-        if accelerator == 'cpu': 
-            devices = None
-            
+        self._devices = devices
+        if accelerator == 'cpu':
+            self._devices = None
+
         logger = pl.loggers.TensorBoardLogger(config.logs_path, name=None)
         super().__init__(
             default_root_dir=config.logs_path,
             logger=logger,
             accelerator=accelerator,
-            devices=devices,
+            devices=self._devices,
             max_epochs=max_epochs,
             # TODO: for some reason, a forward pass happens in the model before datamodule creation.
             num_sanity_val_steps=0)
     
     def test(self, **kwargs) -> None:
         """
-        Uses superclass test results, but additionally, saves raw results as a JSON file, and stores the model weights for future use in inference mode.
-        
-        Returns:
-            None
+        Uses superclass test results, but additionally, saves raw results as a JSON file, and stores
+        the model weights for future use in inference mode.
         """
         results = super().test(**kwargs)[0]
-        
-        with open(os.path.join(config.artifacts_path, "results.json"), "w") as f:
-            json.dump(results, f)
-        
+
+        with open(os.path.join(config.artifacts_path, "results.json"), "w") as file:
+            json.dump(results, file)
+
         torch.save(self.model, os.path.join(config.artifacts_path, 'model.pth'))
-        
-        
+
+
 def main():
-    
+    """Instantiate the CLI and launch the trainer."""
     cli = LightningCLI(trainer_class=Trainer)
     cli.trainer.test(model=cli.model, datamodule=cli.datamodule)
-    
-    
+
+
 if __name__ == '__main__':
-    
     main()
