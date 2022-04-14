ea739881217017b944c3749c37c7448214ac55e1
Lionel VINCENT <97742788+A669015@users.noreply.github.com>
2022-04-14 15:32:54 +0200

    Upgrade torch to 1.11.0 (#19)
    * weather_forecast: upgrade torch and adapt nox sessions
    
    * combustion/gnns: upgrade torch and fix datamodule
    
    * combustion/unets: upgrade torch
    
    * ai4sim-ci-cd.yml: improve nox sessions verbosity and cuda support
    
    * weather_forecast: upgrade torch and adapt nox sessions
    
    * combustion/gnns: upgrade torch and fix datamodule
    
    * combustion/unets: upgrade torch
    
    * ai4sim-ci-cd.yml: improve nox sessions verbosity and cuda support
    
    * gnns: fix unsused import
    
    Co-authored-by: rmslm <76530303+rmslm@users.noreply.github.com>

--- tools/nox/nox_ref_file.py
+++ tools/nox/nox_ref_file.py
@@ -18,7 +18,10 @@ This file is generic and aims at:
 # limitations under the License.
 
 import os
+import subprocess
 from pathlib import Path
+import tempfile
+import warnings
 import nox
 
 REPORTS_DIR = ".ci-reports/"
@@ -54,26 +57,66 @@ def _torch_version(req_file: str = 'requirements.txt') -> str:
             an empty string otherwise.
     """
     version = ''
+    cuda = ''
     with open(req_file, encoding='utf-8') as file:
         for line in file.readlines():
             if "torch==" in line:
                 version = line.rstrip().split('==')[1]
-    return version
+                if "+" not in version:
+                    cuda = "cpu"
+                else:
+                    version, cuda = version.split('+')
+    return version, cuda
+
+
+def _is_cuda_available() -> bool:
+    """Execute the nvidia-smi command to test if cuda is available.
+
+    Returns:
+        (bool): True if cuda is available.
+    """
+    try:
+        out = subprocess.run("nvidia-smi", stdout=subprocess.DEVNULL)
+        if out.returncode == 0:
+            return True
+        return False
+    except FileNotFoundError:
+        return False
 
 
 @nox.session
 def dev_dependencies(session):
     """Target to install all requirements of the use-case code."""
-    torch_vers = _torch_version()
+    torch_vers, cuda_vers = _torch_version()
+    req_file = "requirements.txt"
+
     additional_url = ''
+    extra_url = ''
     if torch_vers:
-        additional_url = f"https://data.pyg.org/whl/torch-{torch_vers}+cpu.html"
+        # If cuda is not available force the CPU mode of torch
+        if not _is_cuda_available():
+            warnings.warn("Cuda not available, torch version forced to CPU mode.")
+            with open(req_file, "r+") as file:
+                req = file.read()
+                req = req.replace(f"torch=={torch_vers}+{cuda_vers}", f"torch=={torch_vers}")
+
+                # Create a temporary requirement file updated with cpu forced torch version
+                req_file = tempfile.NamedTemporaryFile().name
+                with open(req_file, "w+") as new_req:
+                    new_req.writelines(req)
+            cuda_vers = 'cpu'
+
+        # Set the url of precompiled torch version depending on CUDA version
+        if cuda_vers is not "cpu":
+            extra_url = f"https://download.pytorch.org/whl/{cuda_vers}"
+
+        # Set the url of precompiled torch-geometric dependencies depending on torch version
+        additional_url = f"https://data.pyg.org/whl/torch-{torch_vers}+{cuda_vers}.html"
+
     session.install("--upgrade", "pip")
-    session.install("-r", "requirements.txt",
-                    "-f", additional_url)
-    # FIXME: workaround because of incompatibility between setuptools >= 60 and torch < 1.11
-    # cf. https://github.com/pytorch/pytorch/pull/69904#issuecomment-1024338333
-    session.install("setuptools==59.5.0")
+    session.install("-r", req_file,
+                    "-f", additional_url,
+                    "--extra-index-url", extra_url)
 
 
 @nox.session
