fd7e2cd02133978d323de95d2ddcd0d96b1a066c
root <rami.salem@atos.net>
2022-04-19 15:09:38 +0000

    finalize dockerfile

--- docker/script.sh
+++ docker/script.sh
@@ -1,19 +1,28 @@
-#!/bin/sh
+#!/bin/bash
 
 USE_CASE=$1
 
-if [ "$USE_CASE" == combustion_gnn ] 
+echo $USE_CASE
+echo "****************"
+
+if [[ $USE_CASE==combustion_gnns ]] 
 then
-    USE_CASE_PATH = model-collection/combustion/gnns
-elif [ "$USE_CASE" == combustion_unet ] 
+    USE_CASE_PATH=model-collection/combustion/gnns
+elif [[ $USE_CASE==combustion_unets ]] 
 then
-    USE_CASE_PATH = model-collection/combustion/unets
-elif [ "$USE_CASE" == wf_gwd ]
+    USE_CASE_PATH=model-collection/combustion/unets
+elif [[ $USE_CASE==wf_gwd ]]
 then
-    USE_CASE_PATH = model-collection/weather_forecast/gwd
+    USE_CASE_PATH=model-collection/weather_forecast/gwd
 else
+    USE_CASE_PATH=""
     echo "use-case not implemented !"
 fi
+
+
+echo $USE_CASE_PATH
+echo "**********"
+
 
 # install requirements
 python -m pip install -r $USE_CASE_PATH/requirements.txt \
