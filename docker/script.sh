390347a1969ac09f1d97740738c966b77680e4bc
root <rami.salem@atos.net>
2022-04-20 11:51:10 +0000

    fix-script bug

--- docker/script.sh
+++ docker/script.sh
@@ -2,27 +2,15 @@
 
 USE_CASE=$1
 
-echo $USE_CASE
-echo "****************"
-
-if [[ $USE_CASE==combustion_gnns ]] 
-then
-    USE_CASE_PATH=model-collection/combustion/gnns
-elif [[ $USE_CASE==combustion_unets ]] 
-then
-    USE_CASE_PATH=model-collection/combustion/unets
-elif [[ $USE_CASE==wf_gwd ]]
-then
-    USE_CASE_PATH=model-collection/weather_forecast/gwd
+if [ "$USE_CASE" == 'combustion_gnns' ]; then
+    USE_CASE_PATH="model-collection/combustion/gnns"
+elif [ "$USE_CASE" == "combustion_unets" ]; then
+    USE_CASE_PATH="model-collection/combustion/unets"
+elif [ "$USE_CASE" == "wf_gwd" ]; then
+    USE_CASE_PATH="model-collection/weather_forecast/gwd"
 else
-    USE_CASE_PATH=""
     echo "use-case not implemented !"
 fi
-
-
-echo $USE_CASE_PATH
-echo "**********"
-
 
 # install requirements
 python -m pip install -r $USE_CASE_PATH/requirements.txt \
