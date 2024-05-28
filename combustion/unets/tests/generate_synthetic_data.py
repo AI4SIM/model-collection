2cef7d69b6436fe8eb422584ca6f6d452ef18f66
Marradi, Luca <luca.marradi.external@atos.net>
2024-05-28 11:56:45 +0200

    Check and create data fake folder under the root use case folder

--- combustion/unets/tests/generate_synthetic_data.py
+++ combustion/unets/tests/generate_synthetic_data.py
@@ -10,35 +10,28 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from os.path import join, exists
-from os import makedirs, listdir, environ
+from os.path import join, exists, dirname,  realpath
+from os import makedirs, listdir
 from h5py import File
 from yaml import dump, safe_load
 from tempfile import mkdtemp
-from shutil import rmtree
+from shutil import rmtree, move
 from numpy import zeros
 
-
-def read_config(file: str) -> str:
-    """Read the data fake config file"""
-    with open(file, 'r') as file:
-        data = safe_load(file)
-         
-    return data['data']['test_path']
 
 def create_data(tempdir):
     """Create data folder with fake raw data"""
     filenames = ['DNS1_00116000.h5', 'DNS1_00117000.h5', 'DNS1_00118000.h5']
     
-    if (not exists(tempdir)):
+    if (not exists(join(tempdir,'data'))):
         makedirs(join(tempdir, "data","raw"))
     folder = join(tempdir, "data", "raw")
 
     for file_h5 in filenames:
         with File(join(tempdir, "data", "raw", file_h5), 'w') as f:
-            f['filt_8'] = zeros((10, 10, 10))
-            f['filt_grad_8'] = zeros((10, 10, 10))
-            f['grad_filt_8'] = zeros((10, 10, 10))
+            f['filt_8'] = zeros((320, 160, 160))
+            f['filt_grad_8'] = zeros((320, 160, 160))
+            f['grad_filt_8'] = zeros((320, 160, 160))
 
     temp_file_path = join(tempdir, 'data', 'filenames.yaml')
     with open(temp_file_path, 'w') as tmpfile:
@@ -47,8 +40,12 @@ def create_data(tempdir):
     file = join(tempdir, 'data', 'filenames.yaml')
 
 if __name__ == '__main__':
-    file = 'tests/configs/data.yaml'
-    tempdir = read_config(file)
-    #tempdir = mkdtemp(prefix="test") 
+    
+    tempdir = dirname(realpath(__file__))
     create_data(tempdir)
-    #rmtree(tempdir)
+    dst = "/".join(tempdir.split("/")[:-1])
+
+    # Before moving check if dst folder exists
+    if (exists(join(dst,'data'))):
+         rmtree(join(dst,'data'))
+    move(join(tempdir,'data'), dst)    
