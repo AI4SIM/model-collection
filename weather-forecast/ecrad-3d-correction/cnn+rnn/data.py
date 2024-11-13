Handling normalization parameters and fixing typing syntax

weather-forecast/ecrad-3d-correction/cnn+rnn/data.py
@@ -22,7 +22,7 @@ from torch.utils.data import Dataset
 
 from climetlab_maelstrom_radiation.radiation_tf import features_size
 from utils import Keys, VarInfo
-from typing import List, Optional, Union
+from typing import Optional, Union
 
 from nvidia.dali import pipeline_def, Pipeline
 import nvidia.dali.fn as fn
@@ -233,13 +233,13 @@ class RadiationCorrectionDataModule(L.LightningDataModule):
         self,
         cache_dir: str = None,
         train_subset: str = None,
-        train_timestep: List[Union[int, range]] = [0],
-        train_filenum: List[Union[int, range]] = [0],
+        train_timestep: list[Union[int, range]] = [0],
+        train_filenum: list[Union[int, range]] = [0],
         val_subset: str = None,
-        val_timestep: List[Union[int, range]] = [0],
-        val_filenum: List[Union[int, range]] = [0],
-        test_date: List[int] = None,
-        test_timestep: List[Union[int, range]] = None,
+        val_timestep: list[Union[int, range]] = [0],
+        val_filenum: list[Union[int, range]] = [0],
+        test_date: list[int] = None,
+        test_timestep: list[Union[int, range]] = None,
         batch_size: int = 1,
         num_threads: int = 1,
     ):

weather-forecast/ecrad-3d-correction/cnn+rnn/layers.py
@@ -10,14 +10,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from utils import Keys
+from utils import Keys, get_means_and_stds
 
 import torch
 from torch.nn import Module, ZeroPad2d
 from torch.nn.functional import scaled_dot_product_attention
 from torch.nn.attention import SDPBackend, sdpa_kernel
-
-from typing import Dict, List, Tuple
 
 
 class MultiHeadAttention(Module):
@@ -98,7 +96,7 @@ class HRLayer(Module):
         super().__init__()
         self.register_buffer("g_cp", torch.tensor(24 * 3600 * 9.80665 / 1004))
 
-    def forward(self, inputs: List[torch.Tensor]) -> torch.Tensor:
+    def forward(self, inputs: list[torch.Tensor]) -> torch.Tensor:
         # test the input shape
         hlpress = inputs[1]
         net_press = hlpress[..., 1:] - hlpress[..., :-1]
@@ -175,11 +173,11 @@ class ScaLayer(Module):
 
         self.normalization = Normalization(self.sca_mean, self.sca_std)
 
-    def forward(self, x: Dict[str, torch.Tensor]) -> torch.Tensor:
+    def forward(self, x: dict[str, torch.Tensor]) -> torch.Tensor:
         """Compute the forward pass.
 
         Args:
-            x (torch.Tensor): Dictionary containing all the features.
+            x (torch.Tensor): dictionary containing all the features.
 
         Returns:
             torch.Tensor:
@@ -224,11 +222,11 @@ class ColLayer(Module):
 
         self.normalization = Normalization(self.col_mean, self.col_std)
 
-    def forward(self, x: Dict[str, torch.Tensor]) -> torch.Tensor:
+    def forward(self, x: dict[str, torch.Tensor]) -> torch.Tensor:
         """Compute the forward pass.
 
         Args:
-            x (torch.Tensor): Dictionary containing all the features.
+            x (torch.Tensor): dictionary containing all the features.
 
         Returns:
             torch.Tensor:
@@ -267,11 +265,11 @@ class HLLayer(Module):
         self.register_buffer("hl_mean", mean)
         self.register_buffer("hl_std", std)
 
-    def forward(self, x: Dict[str, torch.Tensor]) -> torch.Tensor:
+    def forward(self, x: dict[str, torch.Tensor]) -> torch.Tensor:
         """Compute the forward pass.
 
         Args:
-            x (torch.Tensor): Dictionary containing all the features.
+            x (torch.Tensor): dictionary containing all the features.
 
         Returns:
             torch.Tensor:
@@ -309,11 +307,11 @@ class InterLayer(Module):
 
         self.normalization = Normalization(self.inter_mean, self.inter_std)
 
-    def forward(self, x: Dict[str, torch.Tensor]) -> torch.Tensor:
+    def forward(self, x: dict[str, torch.Tensor]) -> torch.Tensor:
         """Compute the forward pass.
 
         Args:
-            x (torch.Tensor): Dictionary containing all the features.
+            x (torch.Tensor): dictionary containing all the features.
 
         Returns:
             torch.Tensor:
@@ -335,15 +333,13 @@ class PreProcessing(Module):
 
     def __init__(
         self,
-        means: Dict[str, torch.Tensor],
-        stds: Dict[str, torch.Tensor],
-        colum_padding: Tuple[int, int, int, int] = (0, 0, 1, 0),
-        inter_padding: Tuple[int, int, int, int] = (0, 0, 1, 1),
+        path_to_params: str = None,
+        colum_padding: tuple[int, int, int, int] = (0, 0, 1, 0),
+        inter_padding: tuple[int, int, int, int] = (0, 0, 1, 1),
     ) -> None:
         super().__init__()
 
-        self.means = means
-        self.stds = stds
+        self.means, self.stds = get_means_and_stds(path_to_params)
 
         self.sca_layer = ScaLayer(self.means["sca_inputs"], self.stds["sca_inputs"])
         self.col_layer = ColLayer(self.means["col_inputs"], self.stds["col_inputs"])
@@ -355,11 +351,11 @@ class PreProcessing(Module):
         self.zeropad_col = ZeroPad2d(colum_padding)
         self.zeropad_inter = ZeroPad2d(inter_padding)
 
-    def forward(self, x: Dict[str, torch.Tensor]) -> torch.Tensor:
+    def forward(self, x: dict[str, torch.Tensor]) -> torch.Tensor:
         """Compute the forward pass.
 
         Args:
-            x (torch.Tensor): Dictionary containing all the features.
+            x (torch.Tensor): dictionary containing all the features.
 
         Returns:
             torch.Tensor:

weather-forecast/ecrad-3d-correction/cnn+rnn/main.py
@@ -15,7 +15,7 @@ import torch
 from torch.optim import AdamW
 from transformers.optimization import get_cosine_with_min_lr_schedule_with_warmup
 
-from typing import Dict
+from utils import VarInfo, get_total_features
 
 
 class RadiationCorrectionModel(L.LightningModule):
@@ -40,12 +40,17 @@ class RadiationCorrectionModel(L.LightningModule):
         self.fused = fused
         self.num_warmup_steps = num_warmup_steps
 
-    def forward(self, x: torch.Tensor) -> Dict[str, torch.Tensor]:
+        var_info = VarInfo()
+        self.num_levels = var_info.hl_variables["temperature_hl"]["shape"][0]
+        self.num_features = get_total_features(var_info)
+        self.example_input_array = torch.rand(1, self.num_levels, self.num_features)
+
+    def forward(self, x: torch.Tensor) -> dict[str, torch.Tensor]:
         return self.model(x)
 
     def common_step(
-        self, batch: Dict[str, torch.Tensor], batch_idx: int
-    ) -> Dict[str, torch.Tensor]:
+        self, batch: dict[str, torch.Tensor], batch_idx: int
+    ) -> dict[str, torch.Tensor]:
         # x = batch["x"]
         # pressure_hl = batch["pressure_hl"]
         # y_hat = self(x, pressure_hl)
@@ -55,18 +60,18 @@ class RadiationCorrectionModel(L.LightningModule):
         pass
 
     def training_step(
-        self, batch: Dict[str, torch.Tensor], batch_idx: int
-    ) -> Dict[str, torch.Tensor]:
+        self, batch: dict[str, torch.Tensor], batch_idx: int
+    ) -> dict[str, torch.Tensor]:
         return self.common_step(batch, batch_idx)
 
     def validation_step(
-        self, batch: Dict[str, torch.Tensor], batch_idx: int
-    ) -> Dict[str, torch.Tensor]:
+        self, batch: dict[str, torch.Tensor], batch_idx: int
+    ) -> dict[str, torch.Tensor]:
         return self.common_step(batch, batch_idx)
 
     def test_test(
-        self, batch: Dict[str, torch.Tensor], batch_idx: int
-    ) -> Dict[str, torch.Tensor]:
+        self, batch: dict[str, torch.Tensor], batch_idx: int
+    ) -> dict[str, torch.Tensor]:
         return self.common_step(batch, batch_idx)
 
     def optimizer_zero_grad(self, epoch, batch_idx, optimizer):

weather-forecast/ecrad-3d-correction/cnn+rnn/models.py
@@ -18,9 +18,7 @@ import pytorch_lightning as pl
 from torch.nn import LazyConv1d, Linear, Dropout, Module, SiLU, Sequential
 from torch.nn.functional import scaled_dot_product_attention, silu
 from torch.nn.attention import SDPBackend, sdpa_kernel
-from layers import HRLayer, MultiHeadAttention
-
-from typing import Dict
+from layers import HRLayer, MultiHeadAttention, PreProcessing
 
 
 class CNNModel(nn.Module):
@@ -35,6 +33,9 @@ class CNNModel(nn.Module):
         qkv_bias: bool = False,
         attention_dropout: float = 0.0,
         flash_attention: bool = False,
+        colum_padding: tuple[int, int, int, int] = (0, 0, 1, 0),
+        inter_padding: tuple[int, int, int, int] = (0, 0, 1, 1),
+        path_to_params: str = None,
     ):
         super().__init__()
 
@@ -47,11 +48,20 @@ class CNNModel(nn.Module):
         self.qkv_bias = qkv_bias
         self.attention_dropout = attention_dropout
         self.flash_attention = flash_attention
+        self.path_to_params = path_to_params
+        self.colum_padding = colum_padding
+        self.inter_padding = inter_padding
 
         self.save_hyperparameters()
 
-        drates = [2**i for i in self.dilation_rates]
+        # Preprocessing layer
+        self.preprocess = PreProcessing(
+            self.path_to_params, self.colum_padding, self.inter_padding
+        )
+
         # Dilated convolution block to make the information propaate faster
+        drates = [2**i for i in self.dilation_rates]
+        print(f"Using dilation rates: {drates}")
         self.conv_block_with_dilation = Sequential()
         for drate in drates:
             self.conv_with_dilation.append(
@@ -97,9 +107,11 @@ class CNNModel(nn.Module):
         self.hr_lw = HRLayer()
         self.hr_sw = HRLayer()
 
-    def forward(
-        self, x: torch.Tensor, pressure_hl: torch.Tensor
-    ) -> Dict[str, torch.Tensor]:
+    def forward(self, inputs: dict[str, torch.Tensor]) -> dict[str, torch.Tensor]:
+        # Preprocessing
+        x = self.preprocess(inputs)
+
+        # Convolutional layers
         B, T, _ = x.size()  # batch size, sequence length, channels
 
         x = self.conv_block_with_dilation(x)
@@ -112,8 +124,8 @@ class CNNModel(nn.Module):
         sw = self.sw_conv(x)  # B, T, 2
 
         # Heating rate layers
-        hr_lw = self.hr_lw([lw, pressure_hl])
-        hr_sw = self.hr_sw([sw, pressure_hl])
+        hr_lw = self.hr_lw([lw, inputs["pressure_hl"]])
+        hr_sw = self.hr_sw([sw, inputs["pressure_hl"]])
 
         return {
             "hr_sw": hr_sw,

weather-forecast/ecrad-3d-correction/cnn+rnn/utils.py
@@ -10,8 +10,10 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+import torch
+from climetlab_maelstrom_radiation.radiation_tf import NormMerger
 from dataclasses import dataclass, field
-from typing import Dict, Any
+from typing import Any, Dict, Union
 
 
 @dataclass(frozen=True)
@@ -65,6 +67,15 @@ class Keys:
     # Output variables
     output_keys: tuple[str, ...] = ("sw", "lw", "hr_sw", "hr_lw")
 
+    # Packed variables
+    packed_variables: tuple[str, ...] = (
+        "sca_inputs",
+        "col_inputs",
+        "hl_inputs",
+        "inter_inputs",
+        "pressure_hl",
+    )
+
 
 @dataclass(frozen=True)
 class VarInfo:
@@ -115,6 +126,29 @@ class VarInfo:
     )
 
 
+def idx_to_list(idx: Union[int, range]) -> list[int]:
+    """
+    Convert an index to a list of indices.
+    """
+    if isinstance(idx, int):
+        return [idx]
+    elif isinstance(idx, range):
+        return list(idx)
+    else:
+        raise TypeError("Index must be an integer or a range.")
+
+
+def get_total_features(var_info: VarInfo) -> int:
+    """
+    Get the total number of features for raw variables.
+    """
+    total_features = 0
+    for key1 in var_info.keys():
+        for key2 in var_info[key1].keys():
+            total_features += sum(len(idx_to_list(var_info[key1][key2]["idx"])))
+    return total_features
+
+
 def range_deserializer(value: str) -> range:
     """
     Deserialize a string to a range object.
@@ -125,3 +159,18 @@ def range_deserializer(value: str) -> range:
     assert len(arg_list) > 0, "You must at least define the end of the range."
     arg_list = [None if arg == "None" else int(arg) for arg in arg_list]
     return range(*arg_list)
+
+
+def get_means_and_stds(path: str) -> Dict[str, torch.Tensor]:
+    """
+    Get the means and standard deviations of the input variables.
+    """
+    norm_path = glob.glob(os.path.join(path, "*.nc"))
+    norm_ds = NormMerger().to_xarray(norm_path)
+    means = {}
+    stds = {}
+    for k in Keys().packed_variables.keys():
+        means[k] = torch.tensor(norm_ds[f"{k}_mean"].values)
+        stds[k] = torch.tensor(norm_ds[f"{k}_std"].values)
+
+    return means, stds