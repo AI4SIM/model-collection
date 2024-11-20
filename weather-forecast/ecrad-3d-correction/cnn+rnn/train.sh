ea9f5f3c806afa9783e40898256fcffe3246cb72
cbovalo <christophe.bovalo@eviden.com>
2024-11-20 16:38:50 +0100

    Removing shell script

--- weather-forecast/ecrad-3d-correction/cnn+rnn/train.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export LOGDIR=${PWD}
-export CACHE_DIR=/fs1/ECMWF/3dcorrection/data
-export SLURM_NNODES=1
-export SLURM_GPUS_ON_NODE=1
-export PARAMS_PATH=${CACHE_DIR}/train
-
-python trainer.py fit -c configs/cnn.yaml
