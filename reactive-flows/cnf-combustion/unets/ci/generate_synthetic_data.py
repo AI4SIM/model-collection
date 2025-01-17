378ea87ccec31d1d0968d4945e03f4efff953f38
Lionel VINCENT <97742788+A669015@users.noreply.github.com>
2025-01-17 16:36:33 +0100

    SBRA-718 refactor reactive-flows/cnf-combustion/unets to remove config.py (#72)

--- reactive-flows/cnf-combustion/unets/ci/generate_synthetic_data.py
+++ reactive-flows/cnf-combustion/unets/ci/generate_synthetic_data.py
@@ -20,27 +20,26 @@ from h5py import File
 from yaml import dump
 
 sys.path.insert(1, "/".join(os.path.realpath(__file__).split("/")[0:-2]))
-
-import config  # noqa:
+data_path = "./data"
 
 
 def create_data():
     """Create data folder with fake raw data"""
     filenames = ["test_1.h5", "test_2.h5", "test_3.h5"]
 
-    if not exists(config.data_path):
-        makedirs(join(config.data_path, "raw"))
+    if not exists(data_path):
+        makedirs(join(data_path, "raw"))
         for file_h5 in filenames:
-            with File(join(config.data_path, "raw", file_h5), "w") as f:
+            with File(join(data_path, "raw", file_h5), "w") as f:
                 f["filt_8"] = np.random.normal(0, 1, (320, 160, 160))
                 f["filt_grad_8"] = np.random.normal(0, 1, (320, 160, 160))
                 f["grad_filt_8"] = np.random.normal(0, 1, (320, 160, 160))
 
-        temp_file_path = join(config.data_path, "filenames.yaml")
+        temp_file_path = join(data_path, "filenames.yaml")
         with open(temp_file_path, "w") as tmpfile:
             dump(filenames, tmpfile)
     else:
-        raise Exception(f"Remove manually {config.data_path}")
+        raise Exception(f"Remove manually {data_path}")
 
 
 if __name__ == "__main__":
